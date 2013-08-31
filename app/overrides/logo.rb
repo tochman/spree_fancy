e144b9f9657ede8dd29a192b3148566926742731
Thomas ochman <thomas@nocebo.se>
2013-08-31 14:30:23 +0200

    Logo

--- app/overrides/logo.rb
+++ app/overrides/logo.rb
@@ -1,7 +1,7 @@
 Deface::Override.new(:virtual_path => "layouts/spree_application", 
                      :name => "logo", 
                      :replace_contents => "#logo", 
-                     :text => "<img src="store/oh-logo.png" alt="Spree 50"></img>")
+                     :text => "<img src="store'/'oh-logo.png" alt="Spree 50"></img>")
                      
                      
                      
